2ce34ae69b3b464f9dd2e68f357aace9e73d9272
Ravi Solanki <solankiravishankar@gmail.com>
2019-10-03 21:01:38 +0530

    Remove debug toolbar

--- project_sample/urls.py
+++ project_sample/urls.py
@@ -2,7 +2,6 @@ from django.views.generic import TemplateView
 from django.conf.urls import include, url
 
 from django.contrib import admin
-from django.conf import settings
 
 
 admin.autodiscover()
@@ -13,9 +12,3 @@ urlpatterns = [
     url(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
     url(r'^admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
