1875d3bc0e47ef6f7d64fec3c1861d67e51508d3
Leonardo Lazzaro <leonardol@infobytesec.com>
2019-10-27 23:41:04 -0300

    use the latest version of scheduler

--- project_sample/settings.py
+++ project_sample/settings.py
@@ -171,6 +171,7 @@ TEMPLATES = [{
             'django.template.context_processors.i18n',
             'django.template.context_processors.media',
             'django.template.context_processors.request',
+            'django.contrib.messages.context_processors.messages',
         ],
     },
 }]
